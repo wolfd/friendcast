e91f62f7f1918bdd6a6d3bbec35297b537b52c51
cralor <cralor@gmail.com>
2015-02-08 10:07:08 -0500

    fix?

--- server/app.js
+++ server/app.js
@@ -110,7 +110,7 @@ app.post('/reel', urlEncodedParser, function(req, res) {
     var friendIds = new Array();
     graph.get("/me/friends",  function(err, fb) {
         console.log(fb.data);
-        if (fb.data) {
+        if (fb) {
             for (var i = 0; i < fb.data.length; i++) {
                 friendIds[friendIds.length] = fb.data[i].id;
             }
