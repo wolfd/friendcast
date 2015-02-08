52381dd187be571438bbc8508dfb97c2183e66a0
cralor <cralor@gmail.com>
2015-02-08 10:12:16 -0500

    tgnogi

--- server/app.js
+++ server/app.js
@@ -110,9 +110,8 @@ app.post('/reel', urlEncodedParser, function(req, res) {
     var friendIds = new Array();
     graph.get("/me/friends",  function(err, fb) {
         console.log(fb.data);
-        console.log("LENGTH: " + fb.data.length);
         for (var i = 0; i < fb.data.length; i++) {
-            friendIds[i] = fb.data[i].id;
+            friendIds.push(fb.data[i].id);
         }
     });
 
