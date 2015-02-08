5fde0f6d19ef136ed4547400a46675975889ca88
cralor <cralor@gmail.com>
2015-02-08 12:21:03 -0500

    continuous integration is hard

--- server/app.js
+++ server/app.js
@@ -145,7 +145,7 @@ app.post('/reel', urlEncodedParser, function(req, res) {
                                     console.log("I'm done here.", JSON.stringify(returnable));
                                     res.setHeader('Access-Control-Allow-Origin', '*');
                                     res.send(JSON.stringify(returnable));//.end();
-                                    console.log("I'm really done here.", res);
+                                    console.log("I'm really done here.");
                                 }
                             };
 
